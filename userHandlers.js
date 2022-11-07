1fe82fdbe95f882f0e8df2ebf8a4fcb530a9bf02
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2022-11-07 11:04:31 +0100

    post quest express 03 ok

---
+++ userHandlers.js
@@ -0,0 +1,57 @@
+const databaseUsers = require("./databaseUsers");
+
+/** METHODE GET */
+const getUsers = (req, res) => {
+  databaseUsers
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  databaseUsers
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+/** METHODE POST */
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  databaseUsers
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      // wait for it
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  postUser,
+};
